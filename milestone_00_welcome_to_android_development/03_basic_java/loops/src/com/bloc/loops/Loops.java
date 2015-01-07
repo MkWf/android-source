6ed87eefdf3043059f841981666df18a79f0635d
Mark Wassef <markdevw@gmail.com>
2015-01-07 00:02:38 -0500

    Checkpoint 17 assignment completed

--- milestone_00_welcome_to_android_development/03_basic_java/loops/src/com/bloc/loops/Loops.java
+++ milestone_00_welcome_to_android_development/03_basic_java/loops/src/com/bloc/loops/Loops.java
@@ -11,21 +11,16 @@ public class Loops extends Object {
 		boolean temp = false;
 
 		//********************************** Starts Here *********************************
-		temp = someBools[7];
-		someBools[7] = someBools[0];
-		someBools[0] = temp;
+		int i = 7;
+		int j = 0;
 
-		temp = someBools[6];
-		someBools[6] = someBools[1];
-		someBools[1] = temp;
-
-		temp = someBools[5];
-		someBools[5] = someBools[2];
-		someBools[2] = temp;
-
-		temp = someBools[4];
-		someBools[4] = someBools[3];
-		someBools[3] = temp;
+		while(i >= 4) {
+			temp = someBools[i];
+			someBools[i] = someBools[j];
+			someBools[j] = temp;
+			i--;
+			j++; 
+		}
 		//********************************** Ends Here ***********************************
 		
 
@@ -47,14 +42,9 @@ public class Loops extends Object {
 		// learn more here: http://www.cafeaulait.org/course/week2/43.html
 
 		//********************************** Starts Here *********************************
-		numArray[0] = !someBools[0] ? 1 : 0;
-		numArray[1] = !someBools[1] ? 1 : 0;
-		numArray[2] = !someBools[2] ? 1 : 0;
-		numArray[3] = !someBools[3] ? 1 : 0;
-		numArray[4] = !someBools[4] ? 1 : 0;
-		numArray[5] = !someBools[5] ? 1 : 0;
-		numArray[6] = !someBools[6] ? 1 : 0;
-		numArray[7] = !someBools[7] ? 1 : 0;
+		for(int k = 0; k <= 7; k++) {
+			numArray[k] = !someBools[k] ? 1 : 0;
+		}
 		//********************************** Ends Here ***********************************
 
 
