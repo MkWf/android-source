f86dc386a2bbf7661ba9ada7eb5efb77f5a63416
Mark Wassef <markdevw@gmail.com>
2015-01-13 21:20:33 -0500

    checkpoint 35 completed

---
+++ Blocly/app/src/main/java/io/bloc/android/blocly/BloclyApplication.java
@@ -0,0 +1,34 @@
+package io.bloc.android.blocly;
+
+import android.app.Application;
+import io.bloc.android.blocly.api.DataSource;
+
+
+/**
+ * Created by Mark on 1/13/2015.
+ */
+public class BloclyApplication extends Application {
+
+    public static BloclyApplication getSharedInstance() {
+        return sharedInstance;
+    }
+
+    public static DataSource getSharedDataSource() {
+        return BloclyApplication.getSharedInstance().getDataSource();
+    }
+
+    private static BloclyApplication sharedInstance;
+    private DataSource dataSource;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        sharedInstance = this;
+        dataSource = new DataSource();
+    }
+
+    public DataSource getDataSource() {
+        return dataSource;
+    }
+
+}
