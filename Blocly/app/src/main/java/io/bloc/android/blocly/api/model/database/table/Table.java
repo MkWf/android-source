072d32eb7b27cac0c80af901827a9fc71df7d98d
Mark Wassef <markdevw@gmail.com>
2015-01-19 19:28:27 -0500

    checkpoint 52 completed

---
+++ Blocly/app/src/main/java/io/bloc/android/blocly/api/model/database/table/Table.java
@@ -0,0 +1,19 @@
+package io.bloc.android.blocly.api.model.database.table;
+
+import android.database.sqlite.SQLiteDatabase;
+
+/**
+ * Created by Mark on 1/19/2015.
+ */
+public abstract class Table {
+
+    protected static final String COLUMN_ID = "id";
+
+    public abstract String getName();
+
+    public abstract String getCreateStatement();
+
+    public void onUpgrade(SQLiteDatabase writableDatabase, int oldVersion, int newVersion) {
+        // Nothing
+    }
+}
