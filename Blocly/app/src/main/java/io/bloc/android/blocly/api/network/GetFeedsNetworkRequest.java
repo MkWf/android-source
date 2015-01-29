81825ca9249047fab5ca1ff94b660dddcd0b4271
Mark Wassef <markdevw@gmail.com>
2015-01-29 18:23:40 -0500

    Checkpoint 50 assignment completed

--- Blocly/app/src/main/java/io/bloc/android/blocly/api/network/GetFeedsNetworkRequest.java
+++ Blocly/app/src/main/java/io/bloc/android/blocly/api/network/GetFeedsNetworkRequest.java
@@ -28,10 +28,34 @@ public class GetFeedsNetworkRequest extends NetworkRequest {
             try {
                 BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
                 String line = bufferedReader.readLine();
+                int itemCount = 0;
+                int titleTag = 7;
+                int linkTag = 6;
                 while (line != null) {
                     Log.v(getClass().getSimpleName(), "Line: " + line);
+                    int index = 0;
+                    while ((index = line.indexOf("<item>", index)) != -1) {
+                        index++;
+
+                        int startTitle = line.indexOf("<title>", index) + titleTag;
+                        int endTitle = line.indexOf("</title>", index);
+                        char [] title = new char[endTitle - startTitle];
+                        line.getChars(startTitle, endTitle, title, 0);
+                        String itemTitle = new String(title);
+                        Log.i(getClass().getSimpleName(), itemTitle);
+
+                        int startLink = line.indexOf("<link>", index) + linkTag;
+                        int endLink = line.indexOf("</link>", index);
+                        char [] link = new char[endLink - startLink];
+                        line.getChars(startLink, endLink, link, 0);
+                        String itemLink = new String(link);
+                        Log.i(getClass().getSimpleName(), itemLink);
+
+                        itemCount++;
+                    }
                     line = bufferedReader.readLine();
                 }
+                Log.i(getClass().getSimpleName(), "Item Count: " + itemCount);
                 bufferedReader.close();
             } catch (IOException e) {
                 e.printStackTrace();
